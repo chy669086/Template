7e7b49d13906826bae4092c09778c4f91ded3dee
Dynamic_Pigeon <3040313336@qq.com>
2024-05-03 23:16:21 +0800

    debug.h

---
+++ project/algo/debug.h
@@ -0,0 +1,41 @@
+
+#include <format>
+#include <iostream>
+
+inline void debug(char a) {
+  std::cout << a;
+}
+inline void debug(const char *a) {
+  std::cout << a;
+}
+
+#define dprint(s, ...) std::cout << std::format(s, ##__VA_ARGS__)
+
+template <typename T>
+void debug(const T &a) {
+  std::cout << a << " ";
+}
+
+template <typename T>
+  requires requires(T p) {
+    p.begin();
+    p.end();
+    p.begin()++;
+    p.begin() != p.end();
+  }
+void debug(const T &a) {
+  for (auto &x : a) {
+    debug(x);
+  }
+  std::cout << "\n";
+}
+
+template <typename T>
+  requires requires() {
+    T::first;
+    T::second;
+  }
+void debug(const T &a) {
+  std::cout << a.first << " " << a.second;
+  std::cout << "\n";
+}
