324d50bbbc6f101fb453ff32c74cefe29fa08067
Dynamic_Pigeon <3040313336@qq.com>
2024-08-27 15:20:27 +0800

    NTT 更新以及字符串哈希板子现在支持自动扩容

---
+++ Tool/NTT.h
@@ -0,0 +1,94 @@
+
+namespace NTT {
+
+constexpr i64 MOD = 998244353;
+constexpr i64 G = 3;
+
+std::vector<int> tax;
+i64 inv;
+
+static constexpr i64 power(i64 x, i64 y) {
+  i64 res = 1;
+  while (y) {
+    if (y & 1) {
+      res = res * x % MOD;
+    }
+    x = x * x % MOD;
+    y >>= 1;
+  }
+  return res;
+}
+
+void make_rev(int n) {
+  inv = power(n, MOD - 2);
+  tax.resize(n << 1);
+  int d = n >> 1;
+  int p = 0;
+  tax[p++] = 0;
+  tax[p++] = d;
+  for (int w = 2; w <= n; w <<= 1) {
+    d >>= 1;
+    for (int p0 = 0; p0 < w; ++p0) {
+      tax[p++] = tax[p0] | d;
+    }
+  }
+}
+
+void ntt(std::vector<i64> &a) {
+  int n = a.size();
+  for (int i = 1; i < n; i++) {
+    if (i < tax[i]) {
+      std::swap(a[i], a[tax[i]]);
+    }
+  }
+  for (int len = 2, m = 1; len <= n; m = len, len <<= 1) {
+    i64 W = power(G, (MOD - 1) / len);
+    for (auto l = 0, r = len - 1; r <= n; l += len, r += len) {
+      i64 w0 = 1;
+      for (auto p = l, lim = l + m; p < lim; ++p) {
+        i64 x = a[p];
+        i64 y = w0 * a[p + m] % MOD;
+        a[p] = x + y;
+        if (a[p] >= MOD) {
+          a[p] -= MOD;
+        }
+        a[p + m] = x - y;
+        if (a[p + m] < 0) {
+          a[p + m] += MOD;
+        }
+        w0 = W * w0 % MOD;
+      }
+    }
+  }
+}
+
+void intt(std::vector<i64> &a) {
+  ntt(a);
+  std::reverse(a.begin() + 1, a.end());
+  std::ranges::for_each(a, [](i64 &x) {
+    x = x * inv % MOD;
+  });
+}
+
+std::vector<i64> mult(const std::vector<i64> &a, const std::vector<i64> &b) {
+  assert(a.size() == b.size());
+
+  int n = a.size();
+  std::vector<i64> c(n);
+
+  for (int i = 0; i < n; i++) {
+    c[i] = a[i] * b[i] % MOD;
+  }
+  return c;
+}
+
+void mult_self(std::vector<i64> &a, const std::vector<i64> &b) {
+  assert(a.size() == b.size());
+
+  int n = a.size();
+  for (int i = 0; i < n; i++) {
+    a[i] = a[i] * b[i] % MOD;
+  }
+}
+
+} // namespace NTT
--- Tool/字符串哈希.h
+++ Tool/字符串哈希.h
@@ -1,7 +1,6 @@
-
 class StringHash {
 public:
-  StringHash(int n) : n(n) {
+  StringHash(int n = 1) : n(n), mult{{1, 1}} {
     assert(n > 0);
     auto now = std::chrono::system_clock::now();
     auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(
@@ -11,7 +10,7 @@ public:
     do {
       m2 = MULT[(unsigned)(e()) % MULT_SIZE];
     } while (m1 == m2);
-    init_mult();
+    init_mult(n);
   }
 
   std::pair<u64, u64> get_hash(const std::string &s) {
@@ -23,9 +22,9 @@ public:
     return {res1, res2};
   }
 
-  vector<std::pair<u64, u64>> get_hash_set(const std::string &s) {
+  std::vector<std::pair<u64, u64>> get_hash_set(const std::string &s) {
     u64 res1 = 0, res2 = 0;
-    vector<std::pair<u64, u64>> ans;
+    std::vector<std::pair<u64, u64>> ans;
     ans.reserve(s.size());
     for (auto c : s) {
       res1 = (res1 * m1 + c) % MOD[0];
@@ -35,32 +34,39 @@ public:
     return ans;
   }
 
-  void add_hash_set(vector<std::pair<u64, u64>> &a, char c) {
+  void add_hash_set(std::vector<std::pair<u64, u64>> &a, char c) {
     std::pair<u64, u64> tmp = {(a.back().first * m1 + c) % MOD[0],
                                (a.back().second * m2 + c) % MOD[1]};
     a.emplace_back(std::move(tmp));
   }
 
-  bool equal(const vector<std::pair<u64, u64>> &a1, int l1, int r1,
-             const vector<std::pair<u64, u64>> &a2, int l2, int r2) {
+  bool equal(const std::vector<std::pair<u64, u64>> &a1, int l1, int r1,
+             const std::vector<std::pair<u64, u64>> &a2, int l2, int r2) {
 
     std::pair<u64, u64> p1 = get_pair(a1, l1, r1), p2 = get_pair(a2, l2, r2);
 
     return p1 == p2;
   }
 
-  std::pair<u64, u64> get_pair(const vector<std::pair<u64, u64>> &a1, int l1,
+  std::pair<u64, u64> get_pair(const std::vector<std::pair<u64, u64>> &a1, int l1,
                                int r1) {
     // 很乱，我也不知道我在写什么
-    return {
-      (a1[r1].first + MOD[0] -
-       (l1 == 0 ? 0
-                : a1[l1 - 1].first * pow(m1, r1 + 1 - l1, MOD[0]) % MOD[0])) %
-        MOD[0],
-      (a1[r1].second + MOD[1] -
-       (l1 == 0 ? 0
-                : a1[l1 - 1].second * pow(m2, r1 + 1 - l1, MOD[1]) % MOD[1])) %
-        MOD[1]};
+    return 
+    {
+      (
+        a1[r1].first + MOD[0] - 
+        (
+          l1 == 0 ? 0 : a1[l1 - 1].first * pow(r1 + 1 - l1, MOD[0]) % MOD[0]
+        )
+      ) % MOD[0]
+      ,
+      (
+        a1[r1].second + MOD[1] - 
+        (
+          l1 == 0 ? 0 : a1[l1 - 1].second * pow(r1 + 1 - l1, MOD[1]) % MOD[1]
+        )
+      ) % MOD[1]
+    };
   }
 
 private:
@@ -71,18 +77,21 @@ private:
 
   int n;
   u64 m1, m2;
-  vector<std::pair<u64, u64>> mult;
+  std::vector<std::pair<u64, u64>> mult;
 
-  void init_mult() {
-    mult.resize(n + 1);
-    mult[0] = {1, 1};
-    for (int i = 1; i <= n; i++) {
+  void init_mult(int m) {
+    mult.resize(m + 1);
+    for (int i = n; i <= m; i++) {
       mult[i].first = mult[i - 1].first * m1 % MOD[0];
       mult[i].second = mult[i - 1].second * m2 % MOD[1];
     }
+    n = m;
   }
 
-  u64 pow(u64 x, u64 y, u64 mod) {
+  u64 pow(u64 y, u64 mod) {
+    if (n < y) {
+      init_mult(y * 2);
+    }
     return mod == MOD[0] ? mult[y].first : mult[y].second;
   }
 };
