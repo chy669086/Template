37b377c26f766a7136cdef58658daab1765d1521
Dynamic_Pigeon <3040313336@qq.com>
2024-11-24 20:29:35 +0800

    update

--- project/algo/debug.h
+++ project/algo/debug.h
@@ -1,19 +1,17 @@
 
-#include <format>
 #include <iostream>
 
-#define dprint(s, ...) std::cout << std::format(s, ##__VA_ARGS__)
 
 inline void debug(char a) {
-  std::cout << a;
+  std::cerr << a;
 }
 inline void debug(const char *a) {
-  std::cout << a;
+  std::cerr << a;
 }
 
 template <typename T>
 void debug(const T &a) {
-  std::cout << a << " ";
+  std::cerr << a << " ";
 }
 
 template <typename T>
@@ -22,9 +20,14 @@ template <typename T>
     T::second;
   }
 void debug(const T &a) {
-  std::cout << a.first << " " << a.second;
-  std::cout << "\n";
+  std::cerr << a.first << " " << a.second;
+  std::cerr << "\n";
 }
+
+inline void debug() {
+  std::cerr << "\n";
+}
+
 
 template <typename T>
   requires requires(T p) {
@@ -37,5 +40,10 @@ void debug(const T &a) {
   for (auto &x : a) {
     debug(x);
   }
-  std::cout << "\n";
+  std::cerr << "\n";
+}
+
+template <typename... Args>
+void debug(Args... args) {
+  (debug(args), ...);
 }
